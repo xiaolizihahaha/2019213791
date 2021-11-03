be818f4716560f5b7eb713f1ed2ebe7fd5d9f42e
xiaolizihahaha <79213070+2019213791l@users.noreply.github.com>
2021-11-03 22:03:14 +0800

    Add files via upload

---
+++ 02/02静态/index.js
@@ -0,0 +1,242 @@
+function p1mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：Windows程序设计";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周一1-2节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N217";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：赵卫中";
+}
+
+function p2mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：软件工程导论";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周一3-4节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N108";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：李蓉";
+}
+
+function p3mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：电子商务";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周一5-6节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N522";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：刘巍";
+}
+
+function p4mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：解码外交";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周一11-12节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N327";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：胡宗山";
+}
+
+function p5mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：Web程序设计";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周二1-2节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N108";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：涂新辉";
+}
+
+function p6mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：软件项目管理";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周一3-4节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N108";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：李蓉";
+}
+
+function p7mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：设计模式";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周二7-8节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N520";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：李增扬";
+}
+
+function p8mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：人工智能";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周三1-2节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N520";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：张连发";
+}
+
+function p9mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：操作系统";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周三3-4节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N213";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：喻莹";
+}
+
+function p10mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：Windows程序设计";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周四9-10节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N522";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：赵卫中";
+}
+
+function p11mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：习近平新时代中国特色社会主义思想概论";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周四3-4节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N215";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：张艳斌";
+}
+
+function p12mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：Web程序设计";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周四5-6节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N108";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：涂新辉";
+}
+
+function p13mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：编译原理";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周四7-8节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N213";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：郭京蕾";
+}
+
+function p14mouseover()
+{document.getElementById("t2").style.visibility="visible";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：操作系统";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：周五1-2节";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：南湖综合楼N524";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：喻莹";
+}
+
+
+function pmouseout()
+{   document.getElementById("t2").style.visibility="hidden";
+    var info=document.getElementById("name");
+    info.innerText="课程名称：";
+    var when=document.getElementById("when");
+    when.innerText="上课时间：";
+    var where=document.getElementById("where");
+    where.innerText="上课地点：";
+    var who=document.getElementById("who");
+    who.innerText="上课教师：";
+
+}
+
+var p1=document.getElementById("ta1");
+p1.addEventListener('mouseover',p1mouseover);
+p1.addEventListener('mouseout',pmouseout);
+
+var p2=document.getElementById("ta2");
+p2.addEventListener('mouseover',p2mouseover);
+p2.addEventListener('mouseout',pmouseout);
+
+var p3=document.getElementById("ta3");
+p3.addEventListener('mouseover',p3mouseover);
+p3.addEventListener('mouseout',pmouseout);
+
+var p4=document.getElementById("ta4");
+p4.addEventListener('mouseover',p4mouseover);
+p4.addEventListener('mouseout',pmouseout);
+
+var p5=document.getElementById("ta5");
+p5.addEventListener('mouseover',p5mouseover);
+p5.addEventListener('mouseout',pmouseout);
+
+var p6=document.getElementById("ta6");
+p6.addEventListener('mouseover',p6mouseover);
+p6.addEventListener('mouseout',pmouseout);
+
+var p7=document.getElementById("ta7");
+p7.addEventListener('mouseover',p7mouseover);
+p7.addEventListener('mouseout',pmouseout);
+
+var p8=document.getElementById("ta8");
+p8.addEventListener('mouseover',p8mouseover);
+p8.addEventListener('mouseout',pmouseout);
+
+var p9=document.getElementById("ta9");
+p9.addEventListener('mouseover',p9mouseover);
+p9.addEventListener('mouseout',pmouseout);
+
+var p10=document.getElementById("ta10");
+p10.addEventListener('mouseover',p10mouseover);
+p10.addEventListener('mouseout',pmouseout);
+
+var p11=document.getElementById("ta11");
+p11.addEventListener('mouseover',p11mouseover);
+p11.addEventListener('mouseout',pmouseout);
+
+var p12=document.getElementById("ta12");
+p12.addEventListener('mouseover',p12mouseover);
+p12.addEventListener('mouseout',pmouseout);
+
+var p13=document.getElementById("ta13");
+p13.addEventListener('mouseover',p13mouseover);
+p13.addEventListener('mouseout',pmouseout);
+
+var p14=document.getElementById("ta14");
+p14.addEventListener('mouseover',p14mouseover);
+p14.addEventListener('mouseout',pmouseout);
+
+
+
+
+
